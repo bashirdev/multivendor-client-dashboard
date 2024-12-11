7757d85618a0c1f37214d683c4df0d8c7ad2da6f
bashir69 <bashir227476@gmail.com>
2024-12-11 22:45:22 +0400

    update by bashir in Addproduct

--- src/views/seller/AddProduct.jsx
+++ src/views/seller/AddProduct.jsx
@@ -141,8 +141,8 @@ const AddProduct = () => {
         <div className='px-2 lg:px-7 pt-5'>
             <div className='w-full p-4 bg-[#6a5fdf] rounded-md'>
                 <div className='flex justify-between items-center pb-4'>
-                    <h1 className='text-[#d0d2d6] text-xl font-semibold'>Add Product</h1>
-                    <Link to='/seller/dashboard/products' className='bg-blue-500 hover:shadow-blue-500/50 hover:shadow-lg text-white rounded-sm px-7 py-2 my-2'>All Product</Link> 
+                    <h2 className='text-[#d0d2d6] text-xl font-semibold'>Add Product</h2>
+                    <Link to='/seller/dashboard/products' className='bg-blue-500 hover:shadow-blue-500/50 hover:shadow-lg text-white rounded-sm px-7 py-2 my-2'>All Product </Link> 
                 </div>
 <div>
     <form onSubmit={add}>
